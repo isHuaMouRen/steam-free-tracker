9436ecdaced04301886dde6153c292cda0f0260c
HuaMouRen <2825338723@qq.com>
2026-06-10 18:49:59 +0800

    Initialize

---
+++ SteamFreeTracker/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using SteamFreeTracker.Windows;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SteamFreeTracker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var win = new WindowMain();
+            this.MainWindow = win;
+            
+
+
+
+        }
+    }
+
+}
