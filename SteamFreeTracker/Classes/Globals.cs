0c78d0ab0a02d4604e4930be313320be8c4ee377
HuaMouRen <2825338723@qq.com>
2026-06-10 18:53:00 +0800

    feat(global): 添加全局类

---
+++ SteamFreeTracker/Classes/Globals.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SteamFreeTracker.Classes
+{
+    public static class Globals
+    {
+        public static readonly string Version = "1.0.0";
+
+        public static class APIs
+        {
+            public const string FreeGamesAPI = "https://store.steampowered.com/api/featuredcategories?cc=CN&l=zh-cn";
+        }
+    }
+}
